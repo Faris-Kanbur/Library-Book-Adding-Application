edb4528543f34a4f90e633f2f1963c1c6f21a103
faris <faris38kanbur@gmail.com>
2020-10-02 19:52:14 +0200

    feat: add new Js code

--- myScript.js
+++ myScript.js
@@ -59,8 +59,15 @@ document.getElementById("formKitap").addEventListener("submit",function(e){
     const kitap = new Kitap(kitapAdi,kitapYazari,kitapISBN);
 
     const islem = new Arayuz();
-    islem.kitapEkle(kitap);
 
+    if(kitapAdi !="" && kitapYazari !="" && kitapISBN !=""){
+    islem.kitapEkle(kitap);
+    document.getElementById("sonuc").innerHTML="Kitap Basariyla eklenmistir";
+}
+
+else{
+    document.getElementById("sonuc").innerHTML="Bos kisimlar var kitap eklenemedi";
+}
 
     e.preventDefault();
 });
